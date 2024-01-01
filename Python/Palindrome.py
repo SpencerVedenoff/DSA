a9132ae35d4be51ee03a4434f2b7b9019f0de37e
SpencerVedenoff <115957432+SpencerVedenoff@users.noreply.github.com>
2023-12-31 21:49:04 -0800

    Add files via upload

---
+++ Python/Palindrome.py
@@ -0,0 +1,11 @@
+str1 = ["racecar"]
+str2 = ["racecar"]
+
+def solution():
+    str1.reverse()
+    if str1 == str2:
+        print("Palindrome!")
+    else:
+        print("Not Palindrome")
+
+solution()
